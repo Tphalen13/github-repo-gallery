5e665490eec07dd54cf2e33f6b57e9126d141246
Tiffany Phalen <tphalentech@gmail.com>
2022-11-14 15:05:14 -0700

    Added functions to fetch repo data.

--- js/script.js
+++ js/script.js
@@ -30,3 +30,18 @@ const displayUserInfo = function(data){
     overview.append(div);
    
 };
+
+const gitRepos = async function(){
+    const fetchRepos = await fetch();
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function(repos){
+    for(const repo of repos){
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+};
